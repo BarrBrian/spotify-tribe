8a17a0e5b9d89884aff57559b67a540b868d3eb2
Brian Barr <brian.barr23@gmail.com>
2021-04-21 12:01:40 -0400

    resolved the broken redirect issue

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
-  
-  spotifyLogin(){
-    fetch('http://localhost:3000/login')
-      .then(data => {
-        return data.json()
-      })
-      // .then(data => {
-      //   window.open(data);
-      // })
-  }
 
   render() {
     return (
       <div>
         <h1>Project S</h1>
-        <button onClick={this.spotifyLogin} >Login</button>
+        <button onClick={() => window.open('http://localhost:3000/login', '_self')}>Login</button>
       </div>
     )
   }
--- server/models/mainModel.js
+++ server/models/mainModel.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const { Pool } = require('pg');
+
+//NOTE: you may want to configure this with your own URL from ElephantSQL
+const CONNECTION_URL = '';
+
+
 // Your schemas will go here. Replace newSchema with the schema name. 
 const newSchema = new Schema({
   // schema info here
   name: String,
 });
+
+
 
 // The name of the model you will be referencing throughout project, rename schemaModel. 'dbname' should match the database you are using in Mongo.
 const schemaModel = mongoose.model('dbName', newSchema);
